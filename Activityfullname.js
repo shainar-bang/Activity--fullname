73c515228b81db9af946870feb4cfe8f2edc2382
shainar-bang <rabangshaina@gmail.com>
2024-11-04 18:18:18 +0800

    Create Activityfullname.js

---
+++ Activityfullname.js
@@ -0,0 +1,9 @@
+let fullName = "Rabang, Shaina Mae P.";
+let age = 19;
+let favoriteNumber = 25;
+let favoriteColor = "black";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
